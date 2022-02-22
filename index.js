1d44eb93a1c95712747eafbc2b3be9505eb83079
TaylorDarneille <tmdarneille@gmail.com>
2022-02-22 11:32:10 -0800

    posting new dinos

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const app = express()
 app.set('view engine', 'ejs')
 // tell express that we're using ejs layouts
 app.use(ejsLayouts)
+// body-parser middleware
+// this allows us to access form data via req.body
+app.use(express.urlencoded({extended: false}))
 
 // ROUTES
 // home
@@ -24,6 +27,10 @@ app.get('/dinosaurs', (req, res)=>{
     let dinoData = JSON.parse(dinosaurs)
     res.render('index.ejs', {myDinos: dinoData})
 })
+// new route (renders the new dino form)
+app.get('/dinosaurs/new', (req, res)=>{
+    res.render('new.ejs')
+})
 // show ie show all info about a single dino
 // : indicates that the following is a url param(eter)
 app.get('/dinosaurs/:idx', (req, res)=>{
@@ -34,6 +41,20 @@ app.get('/dinosaurs/:idx', (req, res)=>{
     let dinoIndex = req.params.idx
     let targetDino = dinoData[dinoIndex]
     res.render('show.ejs', {dino: targetDino})
+})
+// post a new dino
+app.post('/dinosaurs', (req, res)=>{
+    // read in our dino data from the json file
+    let dinosaurs = fs.readFileSync('./dinosaurs.json')
+    let dinoData = JSON.parse(dinosaurs)
+    // add the new dino to the dinoData array
+    dinoData.push(req.body)
+    // save the dinosaurs to the json file
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+    // redirect back to the index route
+    // res.redirect takes the url pattern for the get route that you
+    // want to run next
+    res.redirect('/dinosaurs')
 })
 
 app.listen(8000, ()=>{
